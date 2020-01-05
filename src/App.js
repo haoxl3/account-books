e55f9bdcc005dc3922434382769688230457125a
haoxl <1098262516@qq.com>
2020-01-05 22:44:48 +0800

    测试配置

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import './App.css'
 import Home from './containers/Home'
+import Create from './containers/Create'
 
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="container pb-5">
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/create" component={Create}></Route>
+                        <Route path="/edit/:id" component={Create}></Route>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
