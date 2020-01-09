e376d1613eb9a2cf5fd6f008f3c91cfa47e17282
haoxl <1098262516@qq.com>
2020-01-09 23:45:37 +0800

    mock

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+axios.get('/items').then(res => {
+    console.log(res)
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
